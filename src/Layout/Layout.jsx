0d8b45b3f5917d6b20d946d1e4282dfe6b056996
lucianoontiveros <luciano.a.ontiveros@gmail.com>
2024-02-28 21:23:53 -0300

    Configuracion de home.jsx

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = () => {
 
   return (
     <>
-      <div className="flex flex-col min-h-screen p-0 min-h-{100vh} bg-[#0d030e] sm:bg-[#3b0764]  md:bg-[#1e1b4b] lg:bg-[#083344]">
+      <div className="flex flex-col min-h-screen p-0 min-h-{100vh} bg-[#0d030e]">
         <Navbar />
         <div>
           {/* Renderiza el componente Loading solo si la navegación está en estado 'loading' */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,20 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/lib/esm/**/*.js',
+    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
-    container: {
-      center: true,
+    extend: {
+      colors: {
+        // Nombres personalizados para tus colores
+        gray: "#909090",
+        skyBlue: "#80DAEB",
+        teal: "#018DA5",
+        mintGreen: "#67DDAB",
+        darkGreen: "#0B9B8A",
+        // Puedes definir más colores según tus necesidades
+      },
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
+  plugins: [require("flowbite/plugin")],
+};
