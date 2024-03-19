46d9af6fa121e262594da85950077131938708d2
lucianoontiveros <luciano.a.ontiveros@gmail.com>
2024-03-19 17:50:18 -0300

    Se implementa solución para Botones de la card presentación que no redireccionaban a los sitios webs correspondientes

--- src/Components/_buttom_travel.jsx
+++ src/Components/_buttom_travel.jsx
@@ -1,32 +1,37 @@
-"use client";
-
 import { Button } from "flowbite-react";
-
 import React from "react";
 
 const _buttom_travel = () => {
   return (
     <div className="flex flex-col gap-2">
-      <Button gradientMonochrome="cyan">
-        <a
-          href="https://discord.gg/UdU3wvpWu7 "
-          target="_blank"
-          rel="noopener noreferrer"
-          alt="discord"
+      <a
+        href="https://discord.gg/UdU3wvpWu7"
+        target="_blank"
+        rel="noopener noreferrer"
+        alt="discord"
+        style={{ display: "inline-block", width: "100%" }}
+      >
+        <Button
+          gradientMonochrome="cyan"
+          style={{ width: "100%" }}
         >
           Discord
-        </a>
-      </Button>
-      <Button gradientMonochrome="purple">
-        <a
-          href="https://www.twitch.tv/cuartodechenz"
-          target="_blank"
-          rel="noopener noreferrer"
-          alt="Twitch"
+        </Button>
+      </a>
+      <a
+        href="https://www.twitch.tv/cuartodechenz"
+        target="_blank"
+        rel="noopener noreferrer"
+        alt="Twitch"
+        style={{ display: "inline-block", width: "100%" }}
+      >
+        <Button
+          gradientMonochrome="purple"
+          style={{ width: "100%" }}
         >
           Twitch
-        </a>
-      </Button>
+        </Button>
+      </a>
     </div>
   );
 };
