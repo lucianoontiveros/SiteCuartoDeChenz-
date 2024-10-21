c35765759a12b1bcc9caeb716bfe9ff140ecd902
lucianoontiveros <luciano.a.ontiveros@gmail.com>
2024-10-21 20:09:04 -0300

    Se soluciona problemas de presentación de todos los apartados.

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [million.vite({ auto: true }), react()],
   base: '/14-react-router-6-4/',  // Ajusta esto al nombre de tu repositorio
-});
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, // Esto permite que las rutas trabajen correctamente en una SPA
+  },
+})
