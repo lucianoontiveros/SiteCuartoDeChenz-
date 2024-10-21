a21c96e50ed65ccd7b49d77c9684762744a3a740
lucianoontiveros <luciano.a.ontiveros@gmail.com>
2024-10-21 19:40:53 -0300

    Se soluciona problemas de presentación de todos los apartados.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import million from "million/compiler";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
- 
+
 export default defineConfig({
   plugins: [million.vite({ auto: true }), react()],
+  base: '/14-react-router-6-4/',  // Ajusta esto al nombre de tu repositorio
 });
